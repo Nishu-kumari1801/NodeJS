eac1e35666bb4fbec1dae7ba3965bb49bbb30be4
Nishu Kumari <nishujha.nj01@gmail.com>
2026-08-18 01:13:44 +0530

    Core Modules and Global Object

---
+++ Revision/01_basic/modules.js
@@ -0,0 +1,12 @@
+// const fs=require('fs');
+// fs.writeFileSync("dummy.txt","trying with modules")
+
+// const os = require('os');
+// console.log(os.platform());
+// console.log(os.hostname());
+// console.log(os.cpus());
+
+//global objects
+console.log("abc");
+console.log(process.cwd());
+console.log(process.pid);
