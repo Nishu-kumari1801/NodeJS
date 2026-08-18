e5e90636fd2d60ab5c607460ea6cab3832bcda49
Nishu Kumari <nishujha.nj01@gmail.com>
2026-08-19 00:16:41 +0530

    External package and package.json

---
+++ Revision/02_CreatingServer/colors.js
@@ -0,0 +1,5 @@
+const colors = require('colors');
+console.log(colors.red("hi , this is nishu jha"));
+console.log(colors.green("hi , this is nishu jha"));
+console.log(colors.yellow("hi , this is nishu jha"));
+console.log(colors.blue("hi , this is nishu jha"));
--- Revision/02_CreatingServer/server.js
+++ Revision/02_CreatingServer/server.js
@@ -1,3 +1,14 @@
+// ==================== lecture:-01(Create Server in Node js) ====================
+
+// const http =require('http');
+// http.createServer((req,res)=>{
+//     // res.write("This is anil sidhu");
+//     res.write('<h1>Hello this is nishu jha</h1>')
+//     res.end("Hello");
+// }).listen(4800);
+
+// ==================== lecture:-02 (External package and package.json) ====================
+
 const http =require('http');
 http.createServer((req,res)=>{
     // res.write("This is anil sidhu");
